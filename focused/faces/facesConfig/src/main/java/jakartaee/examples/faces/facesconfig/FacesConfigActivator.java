9dbc3c641468ba3fb27669c6d3f46862430405a6
Bauke Scholtz <balusc@gmail.com>
2023-05-30 08:43:13 -0400

    JETUT-60: Taking a step further, migrate all BSD0 headers to EPL1 as well after confirmation from Tanja

--- focused/faces/facesConfig/src/main/java/jakartaee/examples/faces/facesconfig/FacesConfigActivator.java
+++ focused/faces/facesConfig/src/main/java/jakartaee/examples/faces/facesconfig/FacesConfigActivator.java
@@ -1,14 +1,13 @@
 /*
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted.
+ * Copyright (c), Eclipse Foundation, Inc. and its licensors.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR(S) DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY
- * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER
- * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE
- * USE OR PERFORMANCE OF THIS SOFTWARE.
+ * All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Distribution License v1.0, which is available at
+ * https://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 package jakartaee.examples.faces.facesconfig;
 
